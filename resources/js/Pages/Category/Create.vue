51d632c59309739335e80a1a8df46386b68f1261
ishikawa976 <hide_ishikawa@outlook.jp>
2025-03-27 21:08:23 +0900

    カテゴリー登録２２

--- resources/js/Pages/Category/Create.vue
+++ resources/js/Pages/Category/Create.vue
@@ -1,4 +1,4 @@
-<script>
+<script setup>
 import InputLabel from "@/Components/InputLabel.vue";
 import TextInput from "@/Components/TextInput.vue";
 import PrimaryButton from '@/Components/PrimaryButton.vue';
@@ -11,22 +11,23 @@ const form = useForm({
 
     const createCategory = () => {
         form.post(route("category.store"));
+        form.name=""
     };
 </script>
 
 <template>
-    <div class="bg-white mx-6 my-6 px-36 py-6">
+    <div class="bg-white mx-12 my-6 px-36 py-6">
         <form @submit.prevent="createCategory">
             カテゴリー登録
-            <InputLabel for="name" value="カテゴリー名" />
+            <InputLabel class="mt-8" for="name" value="カテゴリー名" />
 
             <TextInput 
                 id="name" 
                 type="text"
                 v-model="form.name"
-                class="w-full"
+                class="mt-4 w-full"
             />
-                
+
             <InputError class="mt-2" :message="form.errors.name" />
             <div class="mt-8">
                 <PrimaryButton> 登録 </PrimaryButton>
