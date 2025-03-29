dc61cd5bb7ddf9928d80873163184551742ec166
ishikawa976 <hide_ishikawa@outlook.jp>
2025-03-29 18:53:03 +0900

    編集機能完成

--- resources/js/Pages/Category/Edit.vue
+++ resources/js/Pages/Category/Edit.vue
@@ -4,6 +4,7 @@ import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import InputError from '@/Components/InputError.vue';
 import { useForm } from '@inertiajs/vue3';
+import TextInput from "@/Components/TextInput.vue";
 
 const props = defineProps({
         categories: Array,
@@ -15,22 +16,30 @@ const form = useForm({
 });
 
     const editCategory = () => {
-        form.post(route("category.update", form.id));
+        console.log(form.id)
+        console.log(form.name)
+        form.put(route("category.update", form.id));
     };
+
 </script>
 
 <template><div class="bg-white mx-12 my-6 px-36 py-6">
     
-        <form @submit.prevent="editCategory">
+        <form @submit.prevent="editCategory()">
             カテゴリー編集
             <InputLabel class="mt-8" for="name" value="カテゴリー名" />
-            <input  class="mt-4 w-full" v-model="form.name" list="categorylist"/>
-             <datalist id="categorylist">
-                <div v-for="category in categories" :key=category.id>
-                    <option>{{ category.name }}</option>
-                </div>
-            </datalist>
+            <select v-model="form.id" class="mt-4 w-full">
+                <option v-for="category in categories" :value = "category.id">{{ category.name }}</option>
+            </select>
             
+            <InputLabel class="mt-8" for="name" value="新しいカテゴリー名" />
+            <TextInput 
+                id="name" 
+                type="text"
+                v-model="form.name"
+                class="mt-4 w-full"
+            />
+
             <InputError class="mt-2" :message="form.errors.name" />
             <div class="mt-8">
                 <PrimaryButton> 登録 </PrimaryButton>
