1de0732ebd20279f4b6c04121d636ac80f9acce2
ishikawa976 <hide_ishikawa@outlook.jp>
2025-03-30 19:36:25 +0900

    メーカー、会社関連のモデル、コントローラー作成

---
+++ app/Http/Controllers/MakerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MakerCategory;
+use Illuminate\Http\Request;
+
+class MakerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MakerCategory $makerCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MakerCategory $makerCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, MakerCategory $makerCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MakerCategory $makerCategory)
+    {
+        //
+    }
+}
