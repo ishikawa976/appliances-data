f7566fd72bdf7389cc66bb92c2dc77a745f3e38e
ishikawa976 <hide_ishikawa@outlook.jp>
2025-04-08 22:07:21 +0900

    家電データ登録画面

--- app/Http/Controllers/ApplianceController.php
+++ app/Http/Controllers/ApplianceController.php
@@ -16,7 +16,8 @@ class ApplianceController extends Controller
      */
     public function index()
     {
-        //
+        $appliances = Appliance::all();
+        return Inertia::render('appliance/index',['appliances' => $appliances]);
     }
 
     /**
@@ -25,8 +26,8 @@ class ApplianceController extends Controller
     public function create()
     {
         $categories = Category::orderby('name', 'asc')->get();
-        $makers = Maker::orderby('maker_name', 'asc')->get();
-        $shops = Shop::orderby('full_name', 'asc')->get();
+        $makers = Maker::orderby('name', 'asc')->get();
+        $shops = Shop::orderby('shop_name', 'asc')->get();
         
         return Inertia::render('Appliance/create',
         [
@@ -73,7 +74,8 @@ class ApplianceController extends Controller
      */
     public function show(Appliance $appliance)
     {
-        //
+        $appliance = Appliance::find($id);
+        return Inertia::render('appliance/show',['appliance' => $appliance]);
     }
 
     /**
@@ -81,7 +83,16 @@ class ApplianceController extends Controller
      */
     public function edit(Appliance $appliance)
     {
-        //
+        $appliance = Appliance::find($id);
+        $categories = Category::orderby('name', 'asc')->get();
+        $makers = Maker::orderby('maker_name', 'asc')->get();
+        $shops = Shop::orderby('full_name', 'asc')->get();
+        return Inertia::render('appliance/edit', [
+            'appliance'=>$appliance,
+            'categories'=>$categories,
+            'makers'=>$makers,
+            'shops'=>$shops
+        ]);
     }
 
     /**
@@ -89,14 +100,38 @@ class ApplianceController extends Controller
      */
     public function update(Request $request, Appliance $appliance)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required',
+            'category_id' => 'required',
+            'item_number' => '',
+            'serial_number' => '',
+            'maker_id' => '',
+            'maker_url' => '',
+            'purchase_date' => '',
+            'shop_id' => '',
+            'disposal' => 'required',
+            'disposal_date' => '',
+            'manual_pdf' => '',
+        ]);
+
+        if($request('manual_pdf')) {
+            $filename = $request->file('manual_pdf')->$request('item_number').'.pdf';
+            $request->file('manual_pdf')->storeAs('manual', $filename, 'public');
+        }
+
+        $appliance->Appliance::update($validated);
+
+        return redirect()->route('appliance.index');
     }
+    
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Appliance $appliance)
     {
-        //
+        $appliance->delete();
+
+        return redirect()->route('appliance.index');
     }
 }
---
+++ resources/js/Pages/Appliance/create.vue
@@ -0,0 +1,135 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import InputLabel from "@/Components/InputLabel.vue";
+import TextInput from "@/Components/TextInput.vue";
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import InputError from '@/Components/InputError.vue';
+import { Head, useForm } from '@inertiajs/vue3';
+import { ref } from "vue";
+
+const isDisposal = ref('所蔵');
+const props = defineProps({
+       categories: Array,
+       makers: Array,
+       shops: Array
+});
+
+const form = useForm({
+    name: "",
+    category_id: "",
+    item_number: "",
+    serial_number: "",
+    manufacture_year: "",
+    maker_id: "",
+    maker_url: "",
+    purchase_date: "",
+    shop_id: "",
+    disposal: false,
+    disposal_date: "",
+    manual_pdf: "",
+});
+
+    const createAppliance = () => {
+        form.post(route("appliance.store"));
+        console.log(form)
+    };
+</script>
+
+<template>
+    <AuthenticatedLayout>
+        <Head title="Appliance" />
+        <template #header>
+            <h2
+                class="text-xl font-semibold leading-tight text-gray-800"
+            >
+                家電データ登録
+            </h2>
+        </template>
+        <div class="bg-white mx-48 my-6 px-60 py-6">
+             <form @submit.prevent="createAppliance">
+                <div>
+                    <InputLabel for="name" value="品名" />
+                    <TextInput
+                        id="name" 
+                        type="text"
+                        v-model="form.name"
+                        class="w-full"
+                    />
+                </div>
+                <div>
+                    <InputLabel for="category_id" value="カテゴリー" />
+                    <select v-model="form.category_id" class="w-full">
+                        <option v-for="category in categories" :value = "category.id">{{ category.name }}</option>
+                    </select>
+                </div>
+                <div>
+                    <InputLabel for="item_number" value="品番" />
+                    <TextInput
+                        id="item_number" 
+                        type="text"
+                        v-model="form.item_number"
+                        class="w-full"
+                    />
+                </div>
+                <div>
+                    <InputLabel for="serial_number" value="製造番号" />
+                    <TextInput
+                        id="serial_number" 
+                        type="text"
+                        v-model="form.serial_number"
+                        class="w-full"
+                    />
+                </div>
+                <div>
+                    <InputLabel for="manufacture_year" value="製造年" />
+                    <TextInput
+                        id="manufacture_year" 
+                        type="number"
+                        v-model="form.manufacture_year"
+                        class="w-full"
+                    />
+                </div>
+                <div>
+                    <InputLabel for="maker_id" value="メーカー" />
+                    <select v-model="form.maker_id" class="w-full">
+                        <option v-for="maker in makers" :value = "maker.id">{{ maker.name }}</option>
+                    </select>
+                </div>
+                <div>
+                    <InputLabel for="maker_url" value="メーカーURL" />
+                    <TextInput
+                        id="maker_url" 
+                        type="text"
+                        v-model="form.maker_url"
+                        class="w-full"
+                    />
+                </div>
+                <div>
+                    <InputLabel for="purchase_date" value="購入日" />
+                    <TextInput
+                        id="purchase_date" 
+                        type="date"
+                        v-model="purchase_date"
+                        class="w-full"
+                    />
+                </div>
+                <div>
+                    <InputLabel for="shop_id" value="購入店舗" />
+                    <select v-model="form.shop_id" class="w-full">
+                        <option v-for="shop in shops" :value = "shop.id">{{ shop.full_name }}</option>
+                    </select>
+                </div>
+                <div>
+                    <InputLabel for="disposal" value="廃棄" />
+                    <label>
+                    <input
+                        v-model="disposal"
+                        type="checkbox"
+                    />
+                         {{ isDisposal }}
+                    </label>
+                </div>
+             </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
